f4adbd4f26080b3dd8459a0c38efbdfd0120dd3b
racz.csaba <racz.csaba.janos@mechwart.com>
2026-09-24 12:59:54 +0200

    Kész

--- TeendoWebApp_13f_1cs_20260924/Pages/Index.cshtml.cs
+++ TeendoWebApp_13f_1cs_20260924/Pages/Index.cshtml.cs
@@ -13,9 +13,52 @@ namespace TeendoWebApp_13f_1cs_20260924.Pages
             _context = context;
         }
         public IList<Teendo> Teendok { get; set; }
+
+        [BindProperty]
+        public Teendo UjTeendo { get; set; }
+
+
         public void OnGet()
         {
             Teendok = _context.Teendok.ToList();
+        }
+
+
+        public async Task<IActionResult> OnPost()
+        {
+            if (!ModelState.IsValid)
+            {
+                Teendok = _context.Teendok.ToList();
+                return Page();
+            }
+            _context.Teendok.Add(UjTeendo);
+            await _context.SaveChangesAsync();
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostKeszAsync(int id)
+        {
+            var teendo = await _context.Teendok.FindAsync(id);
+
+
+            if (teendo != null)
+            {
+                teendo.Kesz = !teendo.Kesz;
+                await _context.SaveChangesAsync();
+            }
+            return RedirectToPage();
+        }
+
+        public async Task<IActionResult> OnPostTorlesAsync(int id)
+        {
+            var teendo = await _context.Teendok.FindAsync(id);
+            if (teendo != null)
+            {
+                _context.Teendok.Remove(teendo);
+                await _context.SaveChangesAsync();
+                TempData["Uzenet"] = "A teendő sikeresen törölve.";
+            }
+            return RedirectToPage();
         }
     }
 }
