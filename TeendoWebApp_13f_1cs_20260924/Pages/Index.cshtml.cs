d0c4b133c751ad47df4f539c63c698d6ce52c53f
racz.csaba <racz.csaba.janos@mechwart.com>
2026-09-24 11:25:08 +0200

    Táblázat előkészítve

--- TeendoWebApp_13f_1cs_20260924/Pages/Index.cshtml.cs
+++ TeendoWebApp_13f_1cs_20260924/Pages/Index.cshtml.cs
@@ -1,13 +1,21 @@
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using TeendoWebApp_13f_1cs_20260924.Data;
+using TeendoWebApp_13f_1cs_20260924.Models;
 
 namespace TeendoWebApp_13f_1cs_20260924.Pages
 {
     public class IndexModel : PageModel
     {
+        private readonly TeendoDbContext _context;
+        public IndexModel(TeendoDbContext context)
+        {
+            _context = context;
+        }
+        public IList<Teendo> Teendok { get; set; }
         public void OnGet()
         {
-
+            Teendok = _context.Teendok.ToList();
         }
     }
 }
